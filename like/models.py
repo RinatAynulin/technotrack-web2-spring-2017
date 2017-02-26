37a5c351c8bbabc64235c69b16b95e2c564903f8
Rinat Aynulin <rinataynulin@gmail.com>
2017-02-26 15:50:55 +0300

    add: likes

--- like/models.py
+++ like/models.py
@@ -1,5 +1,32 @@
 from __future__ import unicode_literals
 
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
-# Create your models here.
+from core.models import Named, Authored
+
+
+class Like(Authored):
+    target_content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    target_id = models.PositiveIntegerField()
+    target = GenericForeignKey('target_content_type', 'target_id')
+
+    class Meta:
+        unique_together = (('author', 'target_content_type', 'target_id'),)
+        verbose_name = u'like'
+        verbose_name_plural = u'likes'
+
+    def __unicode__(self):
+        return 'Like by {}'.format(self.author)
+
+
+class LikeAble(models.Model):
+    likes = GenericRelation(
+        Like,
+        content_type_field='target_content_type',
+        object_id_field='target_id'
+    )
+
+    class Meta:
+        abstract = True
--- ugc/admin.py
+++ ugc/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
 
+from like.models import Like
 from ugc.models import Post
 
-admin.site.register(Post)
+
+class LikeInLine(GenericTabularInline):
+    model = Like
+    ct_field = "target_content_type"
+    ct_fk_field = "target_id"
+    fk_name = "target"
+
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = LikeInLine,
